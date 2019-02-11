db0d31470ca4eda2690f2e7ca9b62037d8be8ca5
Andy <andrew@wearekick.co.uk>
2019-02-11 12:34:21 +0000

    refactoring urls to es6, tweaking css

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,75 +1,62 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import axios from 'axios';
 
 const baseUrl = `https://api.flickr.com/services/rest/`;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const method = `flickr.interestingness.getList`;
-// const method = `flickr.photos.getRecent`;
-// const method = `flickr.photos.getPopular`;
+const safe = 1;
 const format = `json`;
 const jsonCallback = 1;
 const pageCount = 20;
-const extras = `description, owner_name, tags, url_z, url_o`;
+const extras = `description, owner_name, tags`;
 
 class Api extends Component {
+  state = {
+    items: []
+  }
 
-    // set initial state of elements
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: []
-        }
-    }
+  componentDidMount() {
+    this.request =
+      axios
+        .get(`${baseUrl}?method=${method}&api_key=${API_KEY}&format=${format}&nojsoncallback=${jsonCallback}&per_page=${pageCount}&safe_search=${safe}&extras=${extras}`)
+        .then((result => {
+          this.setState({
+            items: result.data.photos.photo
+          })
+        }))
+  }
 
-    // make API request
-    componentDidMount() {
-      this.request =
-        axios
-          .get(`${baseUrl}?method=${method}&api_key=${API_KEY}&format=${format}&nojsoncallback=${jsonCallback}&per_page=${pageCount}&extras=${extras}`)
-          .then((result => {
-            this.setState({
-              items: result.data.photos.photo
-            })
-          }))
-    }
+  imgUrl(image) {
+    return `http://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`
+  }
 
-    // handle image selection
-    selectImage(selectedImage) {
-        this.setState({
-            selectedImage
-        })
-    }
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="card-container">
+          {this.state.items.length ?
+            this.state.items.map((item, index) =>
+              <div key={index} className="card">
+                <div className="image">
+                  <img alt={item.id} src={this.imgUrl(item)}/>
+                </div>
+                <div className="name">
+                  <h3><a href={this.imgUrl(item)}>{item.title}</a> <br/><span>by <a href={`https://www.flickr.com/photos/${item.owner}`}>{item.ownername}</a></span></h3>
+                </div>
+                <div className="content">
+                  <p>{item.description._content.substr(0, 150)}...</p>
+                </div>
+                <div className="tags"><h4>Tags</h4><p>{item.tags.substr(0, 50)}...</p></div>
+              </div>
 
-    // render the app
-    render() {
-        const { items } = this.state;
-        console.log(items);
-        return (
-          <div className="wrapper">
-            <div className="card-container">
-              {this.state.items.length ?
-                this.state.items.map((item, index) =>
-                  <div key={index} className="card">
-                    <div className="image">
-                      <img alt={item.id} src={item.url_z}/>
-                    </div>
-                    <div className="name">
-                      <h3>{item.title} <br/><span>by {item.ownername}</span></h3>
-                    </div>
-                    <div className="content">
-                      <p>{item.description._content.substr(0, 200)}...</p>
-                    </div>
-                    <div className="tags"><h4>Tags</h4><p>{item.tags.substr(0, 50)}...</p></div>
-                  </div>
-
-                  )
-                : <div>Loading...</div>
-              }
-            </div>
-          </div>
-        )
-    }
+              )
+            : <div>Loading...</div>
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Api;
