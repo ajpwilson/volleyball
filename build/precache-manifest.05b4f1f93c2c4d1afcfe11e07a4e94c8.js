597c41fa8c8ef872afa299cde2f15d32d7a723bd
Andy <andrew@wearekick.co.uk>
2019-02-21 09:00:42 +0000

    Tidying up files, hosting on github

---
+++ build/precache-manifest.05b4f1f93c2c4d1afcfe11e07a4e94c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9afc1fce4d530cf8ebc",
+    "url": "/volleyball/static/js/runtime~main.c9afc1fc.js"
+  },
+  {
+    "revision": "b1bf1a09cf51258d7c98",
+    "url": "/volleyball/static/js/main.b1bf1a09.chunk.js"
+  },
+  {
+    "revision": "e73551dc88978be622b4",
+    "url": "/volleyball/static/js/2.e73551dc.chunk.js"
+  },
+  {
+    "revision": "b1bf1a09cf51258d7c98",
+    "url": "/volleyball/static/css/main.0b59101e.chunk.css"
+  },
+  {
+    "revision": "3b242d6413828e5a92f659e905e31dbd",
+    "url": "/volleyball/index.html"
+  }
+];
